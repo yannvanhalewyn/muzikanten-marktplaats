08985c3ab6e7d8c55534ea5cbe26a1e3f358fe42
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-04-28 20:53:45 +0200

    Setup dutch localisation. Set default locale to :nl

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # rails g devise:install told me to put this TODO: change upon deployment
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
