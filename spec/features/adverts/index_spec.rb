dee9a4652dbdbf899b20d97ac247189c8317f946
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-05-08 20:50:48 +0200

    Improved the advert index page to display the first image linked to the advert. Added the number of comments for each advert on the index page and the how long ago it was created. Add appropriate specs

--- spec/features/adverts/index_spec.rb
+++ spec/features/adverts/index_spec.rb
@@ -13,6 +13,50 @@ RSpec.feature "displaying adverts", type: :feature do
     end
   end
 
+  it "Display's the advert's target price if present" do
+    advert.update_attribute(:price, 30)
+    visit adverts_path
+    within("#advert_#{advert.id}") do
+      expect(page).to have_content("30,00")
+    end
+  end
+
+  it "Doesn't display anything if no price specified" do
+    within("#advert_#{advert.id}") do
+      expect(page).to_not have_content("€")
+    end
+  end
+
+  it "Display's the adverts first image as a thumbnail" do
+    advert.images.create(attributes_for(:image))
+    visit adverts_path
+    within("#advert_#{advert.id}") do
+      expect(page).to have_selector("img[src$='#{advert.images.first.asset.thumb.url}']")
+    end
+  end
+
+  it "Doesn't try to display an image if the ad has none" do
+    within("#advert_#{advert.id}") do
+      expect(page).to_not have_selector("img")
+    end
+  end
+
+  it "display's the how long the advert has been on display" do
+    advert.update_attribute(:created_at, 2.days.ago)
+    visit adverts_path
+    within("#advert_#{advert.id}") do
+      expect(page).to have_content("2 dagen geleden")
+    end
+  end
+
+  it "display's the number of comments there are on the adverts" do
+    advert.comments.create(content: "A comment", user_id: 1)
+    visit adverts_path
+    within("#advert_#{advert.id}") do
+      expect(page).to have_content("1 reactie(s)")
+    end
+  end
+
   it "links to an advert" do
     within("#advert_#{advert.id}") do
       click_link advert.title
