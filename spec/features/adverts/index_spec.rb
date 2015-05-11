1a9bd7354c90a26be33951c11568565efc768261
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-05-11 22:14:09 +0200

    Refactored many index-feature specs as index-view specs as they should be.

--- spec/features/adverts/index_spec.rb
+++ spec/features/adverts/index_spec.rb
@@ -7,13 +7,6 @@ RSpec.feature "displaying adverts", type: :feature do
     let!(:advert) { create(:advert) }
     before { visit adverts_path }
 
-    it "shows all adverts" do
-      within("#advert_#{advert.id}") do
-        expect(page).to have_content(advert.title)
-        expect(page).to have_content(advert.description)
-      end
-    end
-
     it "shows adverts ordered by most recent" do
       newer_advert = create(:advert)
       older_advert = create(:advert)
@@ -22,56 +15,6 @@ RSpec.feature "displaying adverts", type: :feature do
       visit adverts_path
       expect(newer_advert.title).to appear_before(advert.title)
       expect(advert.title).to appear_before(older_advert.title)
-    end
-
-    it "Display's the advert's target price if present" do
-      advert.update_attribute(:price, 30)
-      visit adverts_path
-      within("#advert_#{advert.id}") do
-        expect(page).to have_content("30,00")
-      end
-    end
-
-    it "Doesn't display anything if no price specified" do
-      within("#advert_#{advert.id}") do
-        expect(page).to_not have_content("€")
-      end
-    end
-
-    it "Display's the adverts first image as a thumbnail" do
-      advert.images.create(attributes_for(:image))
-      visit adverts_path
-      within("#advert_#{advert.id}") do
-        expect(page).to have_selector("img[src$='#{advert.images.first.asset.thumb.url}']")
-      end
-    end
-
-    it "Doesn't try to display an image if the ad has none" do
-      within("#advert_#{advert.id}") do
-        expect(page).to_not have_selector("img")
-      end
-    end
-
-    it "display's the how long the advert has been on display" do
-      advert.update_attribute(:created_at, 2.days.ago)
-      visit adverts_path
-      within("#advert_#{advert.id}") do
-        expect(page).to have_content("2 dagen geleden")
-      end
-    end
-
-    it "display's the number of comments there are on the adverts" do
-      advert.comments.create(content: "A comment", user_id: 1)
-      visit adverts_path
-      within("#advert_#{advert.id}") do
-        expect(page).to have_content("1 reactie(s)")
-      end
-    end
-
-    it "display's a custom message if no comments" do
-      within("#advert_#{advert.id}") do
-        expect(page).to have_content("geen reacties")
-      end
     end
 
     it "links to an advert" do
--- spec/views/adverts/index.html.erb_spec.rb
+++ spec/views/adverts/index.html.erb_spec.rb
@@ -7,13 +7,83 @@ describe "adverts/index" do
     allow(view).to receive_messages(:will_paginate => nil)
   end
 
-  it "displays the advert state if not for_sale" do
-    assign(:adverts, [
-      build_stubbed(:advert, state: "sold"),
-      build_stubbed(:advert)
-    ])
-    render
-    expect(rendered).to have_content /verkocht/i
-    expect(rendered).to_not have_content /te koop/i
+  describe "basic information" do
+    before do
+      @adverts = [
+        build_stubbed(:advert, state: "sold"),
+        build_stubbed(:advert)
+      ]
+      assign(:adverts, @adverts)
+    end
+
+    it "shows all adverts" do
+      render
+      advert = @adverts.first
+      expect(rendered).to have_content(advert.title)
+      expect(rendered).to have_content(advert.description)
+      advert = @adverts.last
+      expect(rendered).to have_content(advert.title)
+      expect(rendered).to have_content(advert.description)
+    end
   end
+
+  describe "advert listing" do
+    let(:advert) { build_stubbed(:advert) }
+
+    it "displays the advert's title" do
+      render partial: "advert", locals: { advert: advert }
+      expect(rendered).to have_content(advert.title)
+    end
+
+    it "displays the advert's description" do
+      render partial: "advert", locals: { advert: advert }
+      expect(rendered).to have_content(advert.description)
+    end
+
+    it "display's the advert's target price if present" do
+      advert.price = 30
+      render partial: "advert", locals: { advert: advert }
+      expect(rendered).to have_content("30,00")
+    end
+
+    it "Doesn't display anything if no price specified" do
+      render partial: "advert", locals: { advert: advert }
+      expect(rendered).to_not have_content("€")
+    end
+
+    it "Display's the adverts first image as a thumbnail" do
+      advert.images.build(attributes_for(:image))
+      render partial: "advert", locals: { advert: advert }
+      url = advert.images.first.asset.thumb.url
+      expect(rendered).to have_selector("img[src$='#{url}']")
+    end
+
+    it "Doesn't try to display an image if the ad has none" do
+      render partial: "advert", locals: { advert: advert }
+      expect(rendered).to_not have_selector("img")
+    end
+
+    it "display's the how long the advert has been on display" do
+      advert = create(:advert)
+      advert.update_attribute(:created_at, 2.days.ago)
+      render partial: "advert", locals: { advert: advert }
+      expect(rendered).to have_content("2 dagen geleden")
+    end
+
+    it "display's the number of comments there are on the adverts" do
+      advert.comments.create(content: "A comment", user_id: 1)
+      render partial: "advert", locals: { advert: advert }
+      expect(rendered).to have_content("1 reactie(s)")
+    end
+
+    it "display's a custom message if no comments" do
+      render partial: "advert", locals: { advert: advert }
+      expect(rendered).to have_content("geen reacties")
+    end
+  end
+
+  # it "displays the advert state if not for_sale" do
+  #   expect(rendered).to have_content /verkocht/i
+  #   expect(rendered).to_not have_content /te koop/i
+  # end
 end
