f1eaad96901f72aa709b69edd1345e6ed1682685
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-05-12 05:34:42 +0200

    Refactor show advert's state links in author-options panel to advert helpers, with a method call in the show template for each state

--- app/helpers/advert_helper.rb
+++ app/helpers/advert_helper.rb
@@ -1,2 +1,18 @@
 module AdvertHelper
+  def state_link_or_span state
+    if @advert.send("#{state}?")
+      content_tag :span, t("adverts.state.#{state}")
+    else
+      "LINK"
+      case state
+        when "for_sale"
+          path = set_for_sale_advert_path(@advert)
+        when "reserved"
+          path = reserve_advert_path(@advert)
+        when "sold"
+          path = sell_advert_path(@advert)
+      end
+      link_to t("adverts.state.#{state}"), path, method: :put
+    end
+  end
 end
